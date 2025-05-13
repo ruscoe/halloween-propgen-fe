584be9a23027381310c3381635099263586e2811
ruscoe <danruscoe@protonmail.com>
2025-05-12 21:08:33 -0700

    Fix test.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders page header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Halloween Prop Generator/i);
   expect(linkElement).toBeInTheDocument();
 });
